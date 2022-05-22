5f071f322226ae97822e48db8872d04b8ddbec41
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-22 19:47:00 +0300

    add delete project funtionality for adin

---
+++ delete_project.php
@@ -0,0 +1,30 @@
+<?php
+include 'connect.php';
+global $link;
+
+session_start();
+// check if the user is the professor
+if($_SESSION['user_role'] == 2) {
+  
+  $id = $_GET['id'];
+  $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+  $sql = 'DELETE FROM projects WHERE id = ?';
+  if($stmt = mysqli_prepare($link, $sql)) {
+    mysqli_stmt_bind_param($stmt, 'i', $id);
+    mysqli_stmt_execute($stmt);
+    
+    $_SESSION['success'][] = array('Project deleted');
+    header('Location: ../proiect/projects.php');
+  } else {
+    $_SESSION['errors'][] = array('Could not delete project, try again later');
+  }
+
+  mysqli_stmt_close($stmt);
+} else {
+  header('Location: ../proiect/index.php');
+}
+
+session_write_close();
+mysqli_close($link);
+?>
--- delete_user-admin.php
+++ delete_user-admin.php
@@ -4,24 +4,24 @@ global $link;
 
 // check if the user is the admin
 if($_SESSION['user_role'] != 3) {
-  $id = $_GET['id'];
-  
-$id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
 
-$sql = 'DELETE FROM users WHERE id = ?';
-if($stmt = mysqli_prepare($link, $sql)) {
-  mysqli_stmt_bind_param($stmt, 'i', $id);
-  mysqli_stmt_execute($stmt);
-  
-  header('Location: ../proiect/admin.php');
-} else {
-  echo 'ERROR: Could not able to execute ' . $sql . mysqli_error($link);
-}
+  $id = $_GET['id'];  
+  $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
 
-mysqli_stmt_close($stmt);
-mysqli_close($link);
+  $sql = 'DELETE FROM users WHERE id = ?';
+  if($stmt = mysqli_prepare($link, $sql)) {
+    mysqli_stmt_bind_param($stmt, 'i', $id);
+    mysqli_stmt_execute($stmt);
+    
+    header('Location: ../proiect/admin.php');
+  } else {
+    echo 'ERROR: Could not able to execute ' . $sql . mysqli_error($link);
+  }
+
+  mysqli_stmt_close($stmt);
 } else {
   header('Location: ../proiect/index.php');  
 }
 
+mysqli_close($link);
 ?>
