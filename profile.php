c25fa8a473da28d1d905ef3d59368f687cb846a9
Petrut <ivanoiu.petrut27@yahoo.com>
2022-05-24 15:42:27 +0300

    added css for profile page and made the form validation and group id insert

--- profile.php
+++ profile.php
@@ -5,11 +5,11 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
+  <title>Project selector - Settings</title>
   <link rel="stylesheet" href="styles/general.css" />
-    <link rel="stylesheet" href="styles/colors.css" />
-    <link rel="stylesheet" href="styles/register.css" />
-    <link rel="stylesheet" href="styles/meniu.css" />
+  <link rel="stylesheet" href="styles/colors.css" />
+  <link rel="stylesheet" href="styles/login-register.css" />
+  <link rel="stylesheet" href="styles/style.css" />
 </head>
 
 <body>
@@ -26,8 +26,8 @@
     </nav>
   </header>
 
-  <main>
-    <h2>Settings</h2>
+  <main class='main'>
+    <h1 class="heading--primary">Settings</h1>
     <?php
     if(isset($_SESSION['errors'])) {
       $errors = $_SESSION['errors'];
@@ -48,153 +48,171 @@
       echo '</div>';
       unset($_SESSION['success']);
     }
+    ?>
 
-    echo $_SESSION['user_id'];
-    echo $_SESSION['user_name'];
+    <div class="section__wrapper profile__wrapper">
+      <section>
+        <h2 class="heading--secondary">Your projects</h2>
 
-    if(isset($_SESSION['user_id'])){
-      $user_id = $_SESSION['user_id'];
-      $sql = "SELECT * FROM users WHERE id = '$user_id'";
-      $result = mysqli_query($link, $sql);
-      $row = mysqli_fetch_assoc($result);
-      $group_name = 'SELECT groups.name FROM groups WHERE id = ' . $row['id_group'];
-      $group_name = mysqli_query($link, $group_name);
-      $group_name = mysqli_fetch_assoc($group_name);
-      echo <<<HTML
-      <form
-      action="update_user.php"
-      class="form"
-      method="post"
-      enctype="multipart/form-data"
-      >
-      <!-- <div class="form__field">
-          <label class="form__label" for="id">ID</label> -->
-          <input class="form__input" type="hidden" name="id" id="id" value="$row[id]" />
-        <!-- </div> -->
-        <div class="form__field">
-          <label class="form__label" for="first_name">First name</label>
-          <input class="form__input" type="text" name="first_name" id="first_name" value="$row[first_name]" />
-        </div>
-        <div class="form__field">
-          <label class="form__label" for="last_name">Last name</label>
-          <input class="form__input" type="text" name="last_name" id="last_name" value="$row[last_name]" />
-        </div>
-        <div class="form__field">
-          <label class="form__label" for="email">Email</label>
-          <input class="form__input" type="email" name="email" id="email" value="$row[email]" />
-        </div>
-        <div class="form__field">
-          <label class="form__label" for="password">Password</label>
-          <input class="form__input" type="password" name="password" id="password"/>
-        </div>
-        <div class="form__field">
-          <label class="form__label" for="group">Group</label>
-          <input class="form__input" type="text" name="group" id="group" value="$group_name[name]" />
-        </div>
-        <div class="form__field--btn">
-          <button type="submit" class="form__button btn">Edit</button>
-          <button type="reset" class="form__button btn">Reset</button>
-        </div>
-      </form>
-      HTML;
+        <?php
+        if(isset($_SESSION['user_id'])){
+          $user_id = $_SESSION['user_id'];
+          $sql = "SELECT * FROM users WHERE id = '$user_id'";
+          $result = mysqli_query($link, $sql);
+          $row = mysqli_fetch_assoc($result);
+          $group_name = 'SELECT groups.name FROM groups WHERE id = ' . $row['id_group'];
+          $group_name = mysqli_query($link, $group_name);
+          $group_name = mysqli_fetch_assoc($group_name);
+        }
 
-      // check if is a student
-      if(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 1)) {
-        // select current id for chosen project
-        $sql = "SELECT * FROM chosen_projects WHERE id_user = '$user_id'";
-        $result_chosen_project_id = $link -> query($sql);
+        // check if is a student
+        if(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 1)) {
+          // select current id for chosen project
+          $sql = "SELECT * FROM chosen_projects WHERE id_user = '$user_id'";
+          $result_chosen_project_id = $link -> query($sql);
 
-        $sql = "SELECT projects.id, projects.name, projects.description FROM projects INNER JOIN chosen_projects ON projects.id = chosen_projects.id_project WHERE chosen_projects.id_user = '$user_id' and chosen_projects.status = 0";
-        $result = $link -> query($sql);
-        if($result -> num_rows > 0) {
-          echo '<div class="container">';
-          while($row = $result -> fetch_assoc()) {
-            $row_chosen_projects = $result_chosen_project_id -> fetch_assoc();
-            echo '<div class="project__wrapper">';
-            echo '<div class="project">';
-            echo '<h2 class="project__title accordion">Materie &mdash; ' . $row['name'] . '<ion-icon name="chevron-down-outline" class="icon icon-accordion"></ion-icon></h2>';
-            echo '<p class="project__description">' . $row['description'] . '</p>';
-            echo '</div>';
+          $sql = "SELECT projects.id, projects.name, projects.description FROM projects INNER JOIN chosen_projects ON projects.id = chosen_projects.id_project WHERE chosen_projects.id_user = '$user_id' and chosen_projects.status = 0";
+          $result = $link -> query($sql);
+          if($result -> num_rows > 0) {
+            echo '<div>';
+            while($row = $result -> fetch_assoc()) {
+              $row_chosen_projects = $result_chosen_project_id -> fetch_assoc();
+              echo '<div class="project__wrapper">';
+              echo '<div class="project">';
+              echo '<h2 class="heading--secondary">Materie &mdash; ' . $row['name'] . '<ion-icon name="chevron-down-outline" class="icon icon-accordion"></ion-icon></h2>';
+              echo '<p class="project__description">' . $row['description'] . '</p>';
+              echo '</div>';
 
-            if(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 1)) {
-                echo '<div class="project__buttons">';
-                echo '<a href="complete_project.php?id=' . $row_chosen_projects['id'] . '" class="button button--primary">Complete project</a>';
-                echo '</div>';
+              if(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 1)) {
+                  echo '<div class="project__buttons">';
+                  echo '<a href="complete_project.php?id=' . $row_chosen_projects['id'] . '" class="lnk lnk--project lnk--green"><ion-icon name="checkmark-outline"></ion-icon></a>';
+                  echo '</div>';
+              }
+              echo '</div>';
             }
             echo '</div>';
+          } else {
+            echo '<p>No projects</p>';
           }
-          echo '</div>';
+        }
+      
+
+      // check if profesor
+      if(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 2 || ($_SESSION['user_role'] == 3))) {
+        // in work projects
+        echo '<h4> Projects &ndash; In work </h4>';
+        $sql = "SELECT projects.name, projects.description, users.first_name, users.last_name FROM projects INNER JOIN chosen_projects ON projects.id = chosen_projects.id_project INNER JOIN users ON users.id = chosen_projects.id_user WHERE chosen_projects.status = 0";
+        $result = $link -> query($sql);
+        if($result -> num_rows > 0) {
+          echo<<<HTML
+          <table class="table" border="1">
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Project name</th>
+            <th>Project description</th>
+          </tr>
+          HTML;
+          while($row = $result -> fetch_assoc()) {
+            echo <<<HTML
+            <tr>
+              <td>$row[first_name]</td>
+              <td>$row[last_name]</td>
+              <td>$row[name]</td>
+              <td>$row[description]</td>
+            HTML;
+          }
+        } else {
+          echo '<p>No projects</p>';
+        }
+        
+        //  finished projects
+        echo <<<HTML
+        <h4>Student &ndash; Finished</h4>
+        HTML;
+        $sql = "SELECT projects.name, projects.description, users.first_name, users.last_name FROM projects INNER JOIN chosen_projects ON projects.id = chosen_projects.id_project INNER JOIN users ON users.id = chosen_projects.id_user WHERE chosen_projects.status = 1";
+        $result = $link -> query($sql);
+        if($result -> num_rows > 0) {
+          echo<<<HTML
+          <table class="table" border="1">
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Project name</th>
+            <th>Project description</th>
+          </tr>
+          HTML;
+          while($row = $result -> fetch_assoc()) {
+            echo <<<HTML
+            <tr>
+              <td>$row[first_name]</td>
+              <td>$row[last_name]</td>
+              <td>$row[name]</td>
+              <td>$row[description]</td>
+            HTML;
+          }
         } else {
           echo '<p>No projects</p>';
         }
       }
-    } else {
-      echo 'Nu esti autentificat';
-    }
+      ?>
+      </section>
 
-    // check if profesor
-    if(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 2 || ($_SESSION['user_role'] == 3))) {
-      // in work projects
-      echo '<h4> Projects &ndash; In work </h4>';
-      $sql = "SELECT projects.name, projects.description, users.first_name, users.last_name FROM projects INNER JOIN chosen_projects ON projects.id = chosen_projects.id_project INNER JOIN users ON users.id = chosen_projects.id_user WHERE chosen_projects.status = 0";
-      $result = $link -> query($sql);
-      if($result -> num_rows > 0) {
-        echo<<<HTML
-        <table class="table" border="1">
-        <tr>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Project name</th>
-          <th>Project description</th>
-        </tr>
-        HTML;
-        while($row = $result -> fetch_assoc()) {
+
+      <section>
+        <h2 class="heading--secondary">Update your profile</h2>
+        <?php
+        if(isset($_SESSION['user_id'])){
+          $user_id = $_SESSION['user_id'];
+          $sql = "SELECT * FROM users WHERE id = '$user_id'";
+          $result = mysqli_query($link, $sql);
+          $row = mysqli_fetch_assoc($result);
+          $group_name = 'SELECT groups.name FROM groups WHERE id = ' . $row['id_group'];
+          $group_name = mysqli_query($link, $group_name);
+          $group_name = mysqli_fetch_assoc($group_name);
           echo <<<HTML
-          <tr>
-            <td>$row[first_name]</td>
-            <td>$row[last_name]</td>
-            <td>$row[name]</td>
-            <td>$row[description]</td>
+          <form
+          action="update_user.php"
+          class="form"
+          method="post"
+          enctype="multipart/form-data"
+          >
+            <input class="form__input" type="hidden" name="id" id="id" value="$row[id]" />
+            <div class="form__field">
+              <label class="form__label" for="first_name">First name</label>
+              <input class="form__input" type="text" name="first_name" pattern="[A-Za-z]{3,32}" id="first_name" value="$row[first_name]" required/>
+            </div>
+            <div class="form__field">
+              <label class="form__label" for="last_name">Last name</label>
+              <input class="form__input" type="text" name="last_name" pattern="[A-Za-z]{3,32}" id="last_name" value="$row[last_name]" required/>
+            </div>
+            <div class="form__field">
+              <label class="form__label" for="group">Group</label>
+              <input class="form__input" type="text" name="group" pattern="[1-9]{3}/[1-9]{1}" title="group/semi-group" id="group" value="$group_name[name]" required/>
+            </div>
+            <div class="form__field">
+              <label class="form__label" for="email">Email</label>
+              <input class="form__input" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          title="Enter valid e-mail" id="email" value="$row[email]" required/>
+            </div>
+            <div class="form__field">
+              <label class="form__label" for="password">Password</label>
+              <input class="form__input" type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,64}"
+              title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" id="password"/>
+            </div>
+            <div class="form__field--btn">
+              <button type="submit" class="form__button btn">Edit</button>
+              <button type="reset" class="form__button btn">Reset</button>
+            </div>
+          </form>
           HTML;
         }
-      } else {
-        echo '<p>No projects</p>';
-      }
-      
-      //  finished projects
-      echo <<<HTML
-      <h4>Student &ndash; Finished</h4>
-      HTML;
-      $sql = "SELECT projects.name, projects.description, users.first_name, users.last_name FROM projects INNER JOIN chosen_projects ON projects.id = chosen_projects.id_project INNER JOIN users ON users.id = chosen_projects.id_user WHERE chosen_projects.status = 1";
-      $result = $link -> query($sql);
-      if($result -> num_rows > 0) {
-        echo<<<HTML
-        <table class="table" border="1">
-        <tr>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Project name</th>
-          <th>Project description</th>
-        </tr>
-        HTML;
-        while($row = $result -> fetch_assoc()) {
-          echo <<<HTML
-          <tr>
-            <td>$row[first_name]</td>
-            <td>$row[last_name]</td>
-            <td>$row[name]</td>
-            <td>$row[description]</td>
-          HTML;
-        }
-      } else {
-        echo '<p>No projects</p>';
-      }
-    }
-
-
-    ?>
-
+        ?>
+      </section>
+    </div>
   </main>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
--- update_user.php
+++ update_user.php
@@ -4,7 +4,7 @@ global $link;
 
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
-$group = $_POST['group'];
+$group_name = $_POST['group'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 session_start();
@@ -14,27 +14,37 @@ session_write_close();
 
 $first_name = filter_var($first_name, FILTER_SANITIZE_STRING);
 $last_name = filter_var($last_name, FILTER_SANITIZE_STRING);
-$group = filter_var($group, FILTER_SANITIZE_STRING);
+$group_name = filter_var($group_name, FILTER_SANITIZE_STRING);
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-$group_id = 'SELECT groups.id FROM groups WHERE name = "'.$group.'"';
-$group_id = mysqli_query($link, $group_id);
-$group_id = mysqli_fetch_assoc($group_id);
-$group_id = $group_id['id'];
+$first_name = strtolower($first_name);
+$first_name = ucfirst($first_name);
+$last_name = strtolower($last_name);
+$last_name = ucfirst($last_name);
+
+// select group id from groups table where group name is $group_name
+$sql = "SELECT id FROM groups WHERE name = '$group_name'";
+if($stmt = mysqli_prepare($link, $sql)) {
+    mysqli_stmt_bind_param($stmt, 's', $group_name);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $id_group);
+    mysqli_stmt_fetch($stmt);
+    mysqli_stmt_close($stmt);
+}
 
 if (empty($password)) {
-    $sql = 'UPDATE users SET first_name = ?, last_name = ?, email = ? WHERE id = ?';
+    $sql = 'UPDATE users SET first_name = ?, last_name = ?, id_group = ?, email = ? WHERE id = ?';
 } else {
     $password = filter_var($password, FILTER_SANITIZE_STRING);
     $password = hash('sha256', $password);
-    $sql = 'UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ?';
+    $sql = 'UPDATE users SET first_name = ?, last_name = ?, id_group = ?, email = ?, password = ? WHERE id = ?';
 }
 
 if($stmt = mysqli_prepare($link, $sql)) {
     if (empty($password)) {
-        mysqli_stmt_bind_param($stmt, 'sssi', $first_name, $last_name, $email, $user_id);
+        mysqli_stmt_bind_param($stmt, 'ssssi', $first_name, $last_name, $id_group, $email, $user_id);
     } else {
-        mysqli_stmt_bind_param($stmt, 'ssssi', $first_name, $last_name, $email, $password, $user_id);
+        mysqli_stmt_bind_param($stmt, 'sssssi', $first_name, $last_name, $id_group, $email, $password, $user_id);
     }
     mysqli_stmt_execute($stmt);
 
@@ -42,7 +52,8 @@ if($stmt = mysqli_prepare($link, $sql)) {
     $_SESSION['user_name'] = $first_name;
     $_SESSION['user_email'] = $email;
     $_SESSION['user_password'] = $password;
-    header('Location: ../proiect/settings.php');
+    $_SESSION['user_group'] = $row['id_group'];
+    header('Location: ../proiect/profile.php');
     session_write_close();
 } else {
     echo 'ERROR: Could not able to execute ' . $sql . mysqli_error($link);
